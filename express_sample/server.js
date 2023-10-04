c9e626b549862b03d1c78e6c4c0f5ccfaf021b3e
HOSANG KIM <hosangkim86@outlook.jp>
2023-10-04 11:58:26 +0900

    Update server.js

--- express_sample/server.js
+++ express_sample/server.js
@@ -8,5 +8,24 @@ dotenv.config()
 const HOST = process.env.HOST
 const PORT = process.env.PORT
 
-console.log(HOST)
-console.log(PORT)
+// サーバ作成
+const app = express()
+
+// GETリクエストの処理
+app.get('/', (req, res) => {
+    // リクエストの処理
+    console.log(req.body)
+    console.log(req.url)
+    console.log(req.query)
+
+    // レスポンスの処理
+    res.send('Hello!!!!!!')
+})
+
+//　サーバ停止: 起動中のターミナルで Ctrl + C
+// サーバ待機（Listen）
+app.listen(PORT, HOST, () => {
+    console.log(HOST)
+    console.log(PORT)
+    console.log('wait...')
+})
