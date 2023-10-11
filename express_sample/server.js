ad3f3cdbfcab30d865957bb80e956af4c42a103c
HOSANG KIM <hosangkim86@outlook.jp>
2023-10-11 12:00:32 +0900

    Update server.js

--- express_sample/server.js
+++ express_sample/server.js
@@ -11,6 +11,13 @@ const PORT = process.env.PORT
 // サーバ作成
 const app = express()
 
+// ミドルウェアの設定
+// publicフォルダを静的コンテンツのフォルダに設定
+app.use(express.static(__dirname + '/public'))
+
+// URLエンコード
+app.use(express.urlencoded({ extended: true }))
+
 // GETリクエストの処理
 app.get('/', (req, res) => {
     // リクエストの処理
@@ -20,6 +27,32 @@ app.get('/', (req, res) => {
 
     // レスポンスの処理
     res.send('Hello!!!!!!')
+})
+
+app.get('/profile', (req, res) => {
+    res.send('プロフィール')
+})
+
+// POSTリクエスト
+app.post('/auth', (req, res) => {
+    // POSTデータ取得
+    var loginName = req.body.login_name
+    var password = req.body.password
+    console.log(loginName, password)
+
+    var message = "ログイン失敗"
+    // .env で設定した値で、ログインチェック
+    // TODO：データベースに接続してユーザ取得
+    // TODO：パスワードはハッシュ値でチェック
+    if (loginName == process.env.LOGIN_NAME
+        && password == process.env.PASSWORD) {
+            message = "ログイン成功"
+        //TODO ログインが成功したらユーザの状態を保存
+        //TODO ログイン後のページの転送
+    } else {
+        // TODO ログイン画面に戻す
+    }
+    res.send(message)
 })
 
 //　サーバ停止: 起動中のターミナルで Ctrl + C
